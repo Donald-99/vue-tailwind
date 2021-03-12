a6ddb3497df3ef177561dc327c810e56d11fd14e
Donald-99 <donalddemiraj@newmedia.al>
2021-03-12 13:01:36 +0100

    vue app with tailwind

---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<template>
+     <div class="flex justify-center items-center h-screen">
+        <h1 class="font-bold text-9xl text-emerald-500 text-center">Component</h1>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style lang="scss">
+
+</style>
